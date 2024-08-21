30274cc7ca917292991cd398f191ae6110e509c0
Odeux <Odeux@DESKTOP-NJGP9RU>
2024-08-21 15:11:04 +0300

    Add project files.

---
+++ Ders 1_1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ders_1_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int number1, number2;
+
+            //number1 = 20;
+            //number2 = 30;
+
+            //Console.WriteLine(number1 + number2);
+
+            //Console.Write("Lütfen adınızı yazınız: ");
+            //string ad;
+            //ad = Console.ReadLine();
+
+            //Console.Write("Hoş geldiniz " + ad);
+
+            //string ulke, sehır;
+            //Console.Write("Ülke: ");
+            //ulke = Console.ReadLine();
+            //Console.Write("Şehir: ");
+            //sehır = Console.ReadLine();
+
+            //Console.Write(ulke + " " + sehır);
+
+            // Convert Cast
+            // Convert.DonusumTuru(Donusturulecek deger);
+            //int sayi;
+            //Console.Write("Sayıyı girin: ");
+            //sayi = Convert.ToInt32(Console.ReadLine());
+            //Console.Write(sayi);
+
+            int s1;
+            Console.WriteLine("Sayıyı Girin: ");
+            s1 = Convert.ToInt16(Console.ReadLine());
+
+            Console.Write(s1);
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
